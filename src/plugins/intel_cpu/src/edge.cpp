openvinotoolkit/openvino

Dynamic shape memory reuse solution

src/plugins/intel_cpu/src/edge.cpp
@@ -258,7 +258,7 @@ int Edge::getOutputNum() const {
     return child_port;
 }
 
-void Edge::allocate(const void* mem_ptr) {
+void Edge::allocateCommon(const std::function<void(const MemoryPtr&, const MemoryDesc&)>& allocate) {
     if (status != Status::NeedAllocation)
         return;
 
@@ -272,9 +272,28 @@ void Edge::allocate(const void* mem_ptr) {
 
     auto parentPtr = getParent();
     memoryPtr.reset(new Memory(parentPtr->getEngine()));
-
-    memoryPtr->Create(inputDesc, mem_ptr, false);  // no pads zeroing
+    allocate(memoryPtr, inputDesc);
     status = Status::Allocated;
+}
+
+void Edge::allocate(const void* mem_ptr) {
+    auto allocateFunc = [&](const MemoryPtr& memoryPtr, const MemoryDesc& inputDesc) {
+        memoryPtr->Create(inputDesc, mem_ptr, false);  // no pads zeroing
+    };
+
+    allocateCommon(allocateFunc);
+}
+
+void Edge::allocate(DnnlMemoryMngrPtr memMngr) {
+    if (!memMngr) {
+        IE_THROW(Unexpected) << "Memory manager ptr is NULL";
+    }
+
+    auto allocateFunc = [&](const MemoryPtr& memoryPtr, const MemoryDesc& inputDesc) {
+        memoryPtr->Create(inputDesc, memMngr);
+    };
+
+    allocateCommon(allocateFunc);
 }
 
 std::string Edge::name() const {
@@ -289,34 +308,10 @@ std::string Edge::name() const {
 }
 
 void Edge::externalAllocate(WeightsSharing::Ptr weightsCache) {
-    auto isInPlace = [](const NodePtr node, int port) -> bool {
-        const auto& selected_pd = node->getSelectedPrimitiveDescriptor();
-        if (selected_pd == nullptr)
-            IE_THROW() << "Preferable primitive descriptor is not set.";
-
-        const auto& config = selected_pd->getConfig();
-
-        for (const auto& in : config.inConfs) {
-            if (in.inPlace() == port) {
-                return true;
-            }
-        }
-        for (const auto& out : config.outConfs) {
-            if (out.inPlace() == port) {
-                return true;
-            }
-        }
-
-        return false;
-    };
-
     if (status != Status::NeedAllocation)
         return;
 
-    bool isTheOnlyChildEdgeAtPort = getParent()->getChildEdgesAtPort(getInputNum()).size() == 1;
-    bool isConcurrentUpdatePossible = isInPlace(getParent(), getInputNum()) || isInPlace(getChild(), getOutputNum()) || !isTheOnlyChildEdgeAtPort;
-
-    if (weightsCache && !isConcurrentUpdatePossible) {
+    if (weightsCache) {
         auto alloc = [this] () {
             allocate();
             return memoryPtr;

src/plugins/intel_cpu/src/edge.h
@@ -51,6 +51,7 @@ public:
 
     void init();
     void allocate(const void* mem_ptr = nullptr);
+    void allocate(DnnlMemoryMngrPtr memMngr);
     void externalAllocate(WeightsSharing::Ptr weightsCache);
     void reuse(MemoryPtr ptr);
     void validate();
@@ -104,6 +105,8 @@ private:
 
     EdgePtr getBaseEdge(int look = LOOK_BOTH);
     bool inPlace(LOOK look = LOOK_BOTH);
+    void allocateCommon(const std::function<void(const MemoryPtr&, const MemoryDesc&)>& allocate);
+
     friend class Graph;
 };
 

src/plugins/intel_cpu/src/graph.cpp
@@ -606,35 +606,23 @@ static edge_clusters_t findEdgeClusters(const std::vector<EdgePtr> & graphEdges)
     edge_cluster_idx_map_t edge_cluster_indices;
 
     for (auto &edge : graphEdges) {
-        if (!edge->hasDefinedMaxSize())
-            continue;
-
         auto edge_it = edge_cluster_indices.find(edge);
-
         if (edge_it != edge_cluster_indices.end())
             continue;   // edge is visited
 
         size_t cluster_idx = edge_clusters.size();
         EdgePtr last_shared_edge = nullptr;
-        //has_defined_max_path means all the edges on path from current to the actual shared edge
-        //have defined max memory size so they can be added to the clusters and resolved by mem solver
-        bool has_defined_max_path = true;
 
         // find cluster index
         for (auto shared_edge = edge->getSharedEdge(std::nothrow);
             shared_edge;
             shared_edge = shared_edge->getSharedEdge(std::nothrow)) {
-            has_defined_max_path = has_defined_max_path && shared_edge->hasDefinedMaxSize();
             auto shared_edge_it = edge_cluster_indices.find(shared_edge);
             if (shared_edge_it != edge_cluster_indices.end()) {
                 cluster_idx = shared_edge_it->second;
                 last_shared_edge = shared_edge;
                 break;
             }
-        }
-
-        if (!has_defined_max_path) {
-            continue;
         }
 
         // add shared edges to cluster
@@ -689,22 +677,24 @@ void Graph::AllocateWithReuse() {
 
     const int64_t alignment = 32;  // 32 bytes
 
-    std::vector<MemorySolver::Box> boxes(edge_clusters.size());
+    std::vector<MemorySolver::Box> definedBoxes;
+    std::vector<MemorySolver::Box> undefinedBoxes;
     for (int i = 0; i < edge_clusters.size(); i++) {
-        MemorySolver::Box &box = boxes[i];
-        box = { std::numeric_limits<int>::max(), 0, 0, i };
+        MemorySolver::Box box = { std::numeric_limits<int>::max(), 0, 0, i };
+        int64_t boxSize = 0;
         for (auto &edge : edge_clusters[i]) {
             int e_start = edge->getParent()->execIndex;
             int e_finish = edge->getChild()->execIndex;
 
-            if (!edge->hasDefinedMaxSize()) {
-                IE_THROW() << "Can not allocate memory since the size is undefined.";
+            if (boxSize != -1 && edge->getDesc().hasDefinedMaxSize()) {
+                int64_t e_size = edge->getDesc().getMaxMemSize();  // size in bytes (from the beginning of data to the last element)
+                boxSize = std::max(e_size, boxSize);
+            } else {
+                boxSize = -1;
             }
 
-            int64_t e_size = edge->getDesc().getMaxMemSize();  // size in bytes (from the beginning of data to the last element)
             box.start = std::min(e_start, box.start);
             box.finish = std::max(e_finish, box.finish);
-            box.size =  std::max(e_size, box.size);
         }
 
         // Constant data are filled once on load.
@@ -727,11 +717,17 @@ void Graph::AllocateWithReuse() {
             }
         }
 
-        box.size = div_up(box.size, alignment);
+        if (boxSize != -1) {
+            box.size = div_up(boxSize, alignment);
+            definedBoxes.push_back(box);
+        } else {
+            box.size = boxSize;
+            undefinedBoxes.push_back(box);
+        }
     }
 
-    MemorySolver memSolver(boxes);
-    size_t total_size = static_cast<size_t>(memSolver.solve()) * alignment;
+    MemorySolver staticMemSolver(definedBoxes);
+    size_t total_size = static_cast<size_t>(staticMemSolver.solve()) * alignment;
 
     memWorkspace = std::make_shared<Memory>(eng);
     memWorkspace->Create(DnnlBlockedMemoryDesc(InferenceEngine::Precision::I8, Shape(InferenceEngine::SizeVector{total_size})));
@@ -741,11 +737,11 @@ void Graph::AllocateWithReuse() {
 
     auto* workspace_ptr = static_cast<int8_t*>(memWorkspace->GetData());
 
-    for (int i = 0; i < edge_clusters.size(); i++) {
+    for (auto& box : definedBoxes) {
         int count = 0;
-        for (auto &edge : edge_clusters[i]) {
+        for (auto& edge : edge_clusters[box.id]) {
             if (edge->getStatus() == Edge::Status::NeedAllocation) {
-                int64_t offset = memSolver.getOffset(i);
+                int64_t offset = staticMemSolver.getOffset(box.id);
                 // !! Fallback to individual memory allocation !!
                 // if you like to check infer without reuse just call this function without arguments.
                 edge->allocate(workspace_ptr + offset * alignment);  // alignment in byte
@@ -761,6 +757,40 @@ void Graph::AllocateWithReuse() {
         }
         IE_ASSERT(count == 1);
     }
+
+    if (!undefinedBoxes.empty()) {
+        MemorySolver::normalizeBoxes(undefinedBoxes);
+
+        std::vector<std::vector<MemorySolver::Box>> groups; //groups of nonoverlapping boxes
+        groups.push_back({undefinedBoxes.front()});
+        for (size_t i = 1; i < undefinedBoxes.size(); ++i) {
+            const auto& box = undefinedBoxes[i];
+            bool groupFound = false;
+            for (auto& group : groups) {
+                const auto& lastBox = group.back();
+                if (lastBox.start > box.finish || lastBox.finish < box.start) {
+                    group.push_back(box);
+                    groupFound = true;
+                    break;
+                }
+            }
+
+            if (!groupFound) {
+                groups.push_back({box});
+            }
+        }
+        for (auto& group : groups) {
+            auto grpMemMngr =
+                std::make_shared<DnnlMemoryMngr>(std::unique_ptr<MemoryMngrWithReuse>(new MemoryMngrWithReuse()));
+            for (auto& box : group) {
+                for (auto& edge : edge_clusters[box.id]) {
+                    if (edge->getStatus() == Edge::Status::NeedAllocation) {
+                        edge->allocate(grpMemMngr);
+                    }
+                }
+            }
+        }
+    }
 }
 
 void Graph::Allocate() {
@@ -773,9 +803,6 @@ void Graph::Allocate() {
 
     // Allocate memory space for all edges marked with NeedAllocation
     AllocateWithReuse();
-
-    // Create dummy memory with undefined desc for edges that are need allocation but has not been allocated withing mem solver
-    for (auto& edge : graphEdges) edge->allocate();
 
     // Resolve all other edges with status NotAllocated and in-place
     for (auto& node : graphNodes) node->resolveInPlaceEdges();

riverlijunjie/openvino-1

src/plugins/intel_cpu/src/edge.cpp
@@ -258,7 +258,7 @@ int MKLDNNEdge::getOutputNum() const {
     return child_port;
 }
 
-void MKLDNNEdge::allocate(const void* mem_ptr) {
+void MKLDNNEdge::allocateCommon(const std::function<void(const MKLDNNMemoryPtr&, const MemoryDesc&)>& allocate) {
     if (status != Status::NeedAllocation)
         return;
 
@@ -271,10 +271,30 @@ void MKLDNNEdge::allocate(const void* mem_ptr) {
         IE_THROW() << "Cannot allocate memory for incompatible descriptors.";
 
     auto parentPtr = getParent();
-    memoryPtr.reset(new MKLDNNMemory(parentPtr->getEngine()));
 
-    memoryPtr->Create(inputDesc, mem_ptr, false);  // no pads zeroing
+    memoryPtr.reset(new MKLDNNMemory(parentPtr->getEngine()));
+    allocate(memoryPtr, inputDesc);
     status = Status::Allocated;
+}
+
+void MKLDNNEdge::allocate(const void* mem_ptr) {
+    auto allocateFunc = [&](const MKLDNNMemoryPtr& memoryPtr, const MemoryDesc& inputDesc) {
+        memoryPtr->Create(inputDesc, mem_ptr, false);  // no pads zeroing
+    };
+
+    allocateCommon(allocateFunc);
+}
+
+void MKLDNNEdge::allocate(DnnlMemoryMngrPtr memMngr) {
+    if (!memMngr) {
+        IE_THROW(Unexpected) << "Memory manager ptr is NULL";
+    }
+
+    auto allocateFunc = [&](const MKLDNNMemoryPtr& memoryPtr, const MemoryDesc& inputDesc) {
+        memoryPtr->Create(inputDesc, memMngr);
+    };
+
+    allocateCommon(allocateFunc);
 }
 
 std::string MKLDNNEdge::name() const {
@@ -288,37 +308,11 @@ std::string MKLDNNEdge::name() const {
     return  result.str();
 }
 
-
-
 void MKLDNNEdge::externalAllocate(MKLDNNWeightsSharing::Ptr weightsCache) {
-    auto isInPlace = [](const MKLDNNNodePtr node, int port) -> bool {
-        const auto& selected_pd = node->getSelectedPrimitiveDescriptor();
-        if (selected_pd == nullptr)
-            IE_THROW() << "Preferable primitive descriptor is not set.";
-
-        const auto& config = selected_pd->getConfig();
-
-        for (const auto& in : config.inConfs) {
-            if (in.inPlace() == port) {
-                return true;
-            }
-        }
-        for (const auto& out : config.outConfs) {
-            if (out.inPlace() == port) {
-                return true;
-            }
-        }
-
-        return false;
-    };
-
     if (status != Status::NeedAllocation)
         return;
 
-    bool isTheOnlyChildEdgeAtPort = getParent()->getChildEdgesAtPort(getInputNum()).size() == 1;
-    bool isConcurrentUpdatePossible = isInPlace(getParent(), getInputNum()) || isInPlace(getChild(), getOutputNum()) || !isTheOnlyChildEdgeAtPort;
-
-    if (weightsCache && !isConcurrentUpdatePossible) {
+    if (weightsCache) {
         auto alloc = [this] () {
             allocate();
             return memoryPtr;

src/plugins/intel_cpu/src/edge.h
@@ -51,8 +51,11 @@ public:
 
     void init();
     void allocate(const void* mem_ptr = nullptr);
+
+    void allocate(DnnlMemoryMngrPtr memMngr);
     void externalAllocate(MKLDNNWeightsSharing::Ptr weightsCache);
     void reuse(MKLDNNMemoryPtr ptr);
+
     void validate();
     void drop();
 
@@ -104,6 +107,8 @@ private:
 
     MKLDNNEdgePtr getBaseEdge(int look = LOOK_BOTH);
     bool inPlace(LOOK look = LOOK_BOTH);
+    void allocateCommon(const std::function<void(const MKLDNNMemoryPtr&, const MemoryDesc&)>& allocate);
+
     friend class MKLDNNGraph;
 };
 

src/plugins/intel_cpu/src/graph.cpp
@@ -604,35 +604,23 @@ static edge_clusters_t findEdgeClusters(const std::vector<MKLDNNEdgePtr> & graph
     edge_cluster_idx_map_t edge_cluster_indices;
 
     for (auto &edge : graphEdges) {
-        if (!edge->hasDefinedMaxSize())
-            continue;
-
         auto edge_it = edge_cluster_indices.find(edge);
-
         if (edge_it != edge_cluster_indices.end())
             continue;   // edge is visited
 
         size_t cluster_idx = edge_clusters.size();
         MKLDNNEdgePtr last_shared_edge = nullptr;
-        //has_defined_max_path means all the edges on path from current to the actual shared edge
-        //have defined max memory size so they can be added to the clusters and resolved by mem solver
-        bool has_defined_max_path = true;
 
         // find cluster index
         for (auto shared_edge = edge->getSharedEdge(std::nothrow);
             shared_edge;
             shared_edge = shared_edge->getSharedEdge(std::nothrow)) {
-            has_defined_max_path = has_defined_max_path && shared_edge->hasDefinedMaxSize();
             auto shared_edge_it = edge_cluster_indices.find(shared_edge);
             if (shared_edge_it != edge_cluster_indices.end()) {
                 cluster_idx = shared_edge_it->second;
                 last_shared_edge = shared_edge;
                 break;
             }
-        }
-
-        if (!has_defined_max_path) {
-            continue;
         }
 
         // add shared edges to cluster
@@ -687,22 +675,24 @@ void MKLDNNGraph::AllocateWithReuse() {
 
     const int64_t alignment = 32;  // 32 bytes
 
-    std::vector<MemorySolver::Box> boxes(edge_clusters.size());
+    std::vector<MemorySolver::Box> definedBoxes;
+    std::vector<MemorySolver::Box> undefinedBoxes;
     for (int i = 0; i < edge_clusters.size(); i++) {
-        MemorySolver::Box &box = boxes[i];
-        box = { std::numeric_limits<int>::max(), 0, 0, i };
+        MemorySolver::Box box = { std::numeric_limits<int>::max(), 0, 0, i };
+        int64_t boxSize = 0;
         for (auto &edge : edge_clusters[i]) {
             int e_start = edge->getParent()->execIndex;
             int e_finish = edge->getChild()->execIndex;
 
-            if (!edge->hasDefinedMaxSize()) {
-                IE_THROW() << "Can not allocate memory since the size is undefined.";
+            if (boxSize != -1 && edge->getDesc().hasDefinedMaxSize()) {
+                int64_t e_size = edge->getDesc().getMaxMemSize();  // size in bytes (from the beginning of data to the last element)
+                boxSize = std::max(e_size, boxSize);
+            } else {
+                boxSize = -1;
             }
 
-            int64_t e_size = edge->getDesc().getMaxMemSize();  // size in bytes (from the beginning of data to the last element)
             box.start = std::min(e_start, box.start);
             box.finish = std::max(e_finish, box.finish);
-            box.size =  std::max(e_size, box.size);
         }
 
         // Constant data are filled once on load.
@@ -725,11 +715,17 @@ void MKLDNNGraph::AllocateWithReuse() {
             }
         }
 
-        box.size = div_up(box.size, alignment);
+        if (boxSize != -1) {
+            box.size = div_up(boxSize, alignment);
+            definedBoxes.push_back(box);
+        } else {
+            box.size = boxSize;
+            undefinedBoxes.push_back(box);
+        }
     }
 
-    MemorySolver memSolver(boxes);
-    size_t total_size = static_cast<size_t>(memSolver.solve()) * alignment;
+    MemorySolver staticMemSolver(definedBoxes);
+    size_t total_size = static_cast<size_t>(staticMemSolver.solve()) * alignment;
 
     memWorkspace = std::make_shared<MKLDNNMemory>(eng);
     memWorkspace->Create(DnnlBlockedMemoryDesc(InferenceEngine::Precision::I8, Shape(InferenceEngine::SizeVector{total_size})));
@@ -739,11 +735,12 @@ void MKLDNNGraph::AllocateWithReuse() {
 
     auto* workspace_ptr = static_cast<int8_t*>(memWorkspace->GetData());
 
-    for (int i = 0; i < edge_clusters.size(); i++) {
+    for (auto& box : definedBoxes) {
         int count = 0;
-        for (auto &edge : edge_clusters[i]) {
+        for (auto& edge : edge_clusters[box.id]) {
             if (edge->getStatus() == MKLDNNEdge::Status::NeedAllocation) {
-                int64_t offset = memSolver.getOffset(i);
+                int64_t offset = staticMemSolver.getOffset(box.id);
+
                 // !! Fallback to individual memory allocation !!
                 // if you like to check infer without reuse just call this function without arguments.
                 edge->allocate(workspace_ptr + offset * alignment);  // alignment in byte
@@ -759,6 +756,40 @@ void MKLDNNGraph::AllocateWithReuse() {
         }
         IE_ASSERT(count == 1);
     }
+
+    if (!undefinedBoxes.empty()) {
+        MemorySolver::normalizeBoxes(undefinedBoxes);
+
+        std::vector<std::vector<MemorySolver::Box>> groups; //groups of nonoverlapping boxes
+        groups.push_back({undefinedBoxes.front()});
+        for (size_t i = 1; i < undefinedBoxes.size(); ++i) {
+            const auto& box = undefinedBoxes[i];
+            bool groupFound = false;
+            for (auto& group : groups) {
+                const auto& lastBox = group.back();
+                if (lastBox.start > box.finish || lastBox.finish < box.start) {
+                    group.push_back(box);
+                    groupFound = true;
+                    break;
+                }
+            }
+
+            if (!groupFound) {
+                groups.push_back({box});
+            }
+        }
+        for (auto& group : groups) {
+            auto grpMemMngr =
+                std::make_shared<DnnlMemoryMngr>(std::unique_ptr<MemoryMngrWithReuse>(new MemoryMngrWithReuse()));
+            for (auto& box : group) {
+                for (auto& edge : edge_clusters[box.id]) {
+                    if (edge->getStatus() == MKLDNNEdge::Status::NeedAllocation) {
+                        edge->allocate(grpMemMngr);
+                    }
+                }
+            }
+        }
+    }
 }
 
 void MKLDNNGraph::Allocate() {
@@ -771,9 +802,6 @@ void MKLDNNGraph::Allocate() {
 
     // Allocate memory space for all edges marked with NeedAllocation
     AllocateWithReuse();
-
-    // Create dummy memory with undefined desc for edges that are need allocation but has not been allocated withing mem solver
-    for (auto& edge : graphEdges) edge->allocate();
 
     // Resolve all other edges with status NotAllocated and in-place
     for (auto& node : graphNodes) node->resolveInPlaceEdges();