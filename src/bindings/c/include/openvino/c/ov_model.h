2f249f0d612782e71d633beb42a0604f4d1701ea
ruiqi <ruiqi.yang@intel.com>
2022-08-17 16:50:57 +0800

    Add ov_model_output_by tensor name and index function

--- src/bindings/c/include/openvino/c/ov_model.h
+++ src/bindings/c/include/openvino/c/ov_model.h
@@ -72,6 +72,28 @@ OPENVINO_C_API(ov_status_e)
 ov_model_input_by_index(const ov_model_t* model, const size_t index, ov_output_const_node_t** input_node);
 
 /**
+ * @brief Get the outputs of ov_model_t.
+ * @ingroup model
+ * @param model A pointer to the ov_model_t.
+ * @param index input tensor index.
+ * @param output_node A pointer to the ov_input_node_t.
+ * @return Status code of the operation: OK(0) for success.
+ */
+OPENVINO_C_API(ov_status_e)
+ov_model_output_by_index(const ov_model_t* model, const size_t index, ov_output_const_node_t** output_node);
+
+/**
+ * @brief Get the outputs of ov_model_t.
+ * @ingroup model
+ * @param model A pointer to the ov_model_t.
+ * @param tensor_name input tensor name (char *).
+ * @param output_node A pointer to the ov_output_const_node_t.
+ * @return Status code of the operation: OK(0) for success.
+ */
+OPENVINO_C_API(ov_status_e)
+ov_model_output_by_name(const ov_model_t* model, const char* tensor_name, ov_output_const_node_t** output_node);
+
+/**
  * @brief Returns true if any of the op's defined in the model contains partial shape.
  * @param model A pointer to the ov_model_t.
  */
