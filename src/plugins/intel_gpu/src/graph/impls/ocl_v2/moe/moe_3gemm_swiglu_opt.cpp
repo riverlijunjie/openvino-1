e9b77ba8e68d368f7b6f8e3806eed07815df5e88
River,Li <river.li@intel.com>
2026-01-19 16:30:56 +0800

    Update swiglu kernel

--- src/plugins/intel_gpu/src/graph/impls/ocl_v2/moe/moe_3gemm_swiglu_opt.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl_v2/moe/moe_3gemm_swiglu_opt.cpp
@@ -1620,8 +1620,8 @@ public:
                                       *prefill_swiglu,
                                       {intermediates_memories[MOE_INTERNAL_BUFFER_UP_OUTPUT], intermediates_memories[MOE_INTERNAL_BUFFER_GATE_OUTPUT]},
                                       {intermediates_memories[MOE_INTERNAL_BUFFER_GATE_OUTPUT]},
-                                      {static_cast<size_t>(token_size), static_cast<size_t>(_intermediate_size), 1},
-                                      {1, subgroup_size, 1});
+                                      {static_cast<size_t>(_intermediate_size), static_cast<size_t>(token_size),  1},
+                                      {subgroup_size, 1, 1});
 #    if DUMP_TENSOR_CONTENTS
             {
                 ret_event->wait();  // debug
